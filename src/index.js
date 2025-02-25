c8271d198c5d4254eccd1ebf747d56c510dc7f22
boba-milktea <catherine.idv@gmail.com>
2025-02-25 14:46:52 +0100

    add counter-ui-logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Hello, world!');
+const number = document.getElementById('number');
+const incrementBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
+const resetBtn = document.getElementById('reset');
+
+number.value = 0;
+
+// using increment operator to increase the number
+incrementBtn.addEventListener('click', () => {
+    number.value++;
+});
+
+// using decrement operator to decrease the number
+decrementBtn.addEventListener('click', () => {
+    number.value--;
+    // the number can't be negative
+    number.value < 0 ? (number.value = 0) : null;
+});
+
+reset.addEventListener('click', () => {
+    // confirm with the user if he/she really want to reset the number
+    confirm('Reset the number?') ? (number.value = 0) : null;
+});
